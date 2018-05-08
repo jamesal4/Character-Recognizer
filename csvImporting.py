9f7a707e5e649571e09161d13c68ea61380229a1
James Allen <jamesal4@mit.edu>
2018-05-08 13:50:14 -0400

    combine data and labels into one list

--- csvImporting.py
+++ csvImporting.py
@@ -1,6 +1,9 @@
 import csv
 import numpy as np
 
+allData = []
+allLabels = []
+
 zData = []
 n = 0
 with open('JamesZ.csv') as csvfile:
@@ -12,11 +15,13 @@ with open('JamesZ.csv') as csvfile:
         npRow = np.array(row[:-1],dtype=np.float32)[1:]
         if npRow.all() == np.zeros(6).all():
             n += 1
+            allData.apend(innerArray)
             zData.append(innerArray)
             innerArray = []
         else:
             innerArray.append(npRow)
 zLabels = np.array([0,1]*n)
+allLabels.append([0,1]*n)
 
 nData = []
 n = 0
@@ -34,3 +39,4 @@ with open('JamesN.csv') as csvfile:
         else:
             innerArray.append(npRow)
 nLabels = np.array([1,0]*n)
+allLabels.append([1,0]*n)
