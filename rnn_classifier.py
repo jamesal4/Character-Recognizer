e5220f992a679997732830e7e0b9d8ac8e03f494
Akhil Nistala <anistala@mit.edu>
2018-05-09 19:21:44 -0700

    initial rnn working

--- rnn_classifier.py
+++ rnn_classifier.py
@@ -2,32 +2,81 @@ from csvImporting import allData, allLabels
 import keras
 import numpy as np
 
-### Step 1 - Filter gyroscope/accelerometer data to include relevant datapoints
-
-### Step 2 - run the examples through the network
-
-
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, TimeDistributed
 from keras.utils import to_categorical
-import numpy as np
+
+# from sklearn.metrics import mean_squared_error
+
+
+############# Helper functions
+def unison_shuffle(a, b):
+  assert len(a) == len(b)
+  p = np.random.permutation(len(a))
+  return a[p], b[p]
+
+
+## Step 1 - Filter gyroscope/accelerometer data to include relevant datapoints
+## Step 2 - run the examples through the network
 
 
 
-print allData.shape
-print allLabels.shape
+
+## pad examples so that they are of equal length
+x = []
+y = []
+maxLength = max([len(ex) for ex in allData])
+
+for ix, example in enumerate(allData):
+  example_row = []
+  for val in example:
+    example_row.extend(val)
+
+  pad_length = (maxLength - len(example))*6
+  example_row.extend([0.]*pad_length)
+
+  x.append(np.array(example_row))
+  y.append(np.array(allLabels[ix]))
+
+x = np.array(x) # x.shape = (100, 1476)
+y = np.array(y) # y.shape = (100, 2)
+x = x.reshape(100, 1, 1476)
+
+x, y = unison_shuffle(x, y)
+
+## split data into train/test sets
+numTrainExamples = int(.7*len(x))
+trainX, trainY = x[:numTrainExamples], y[:numTrainExamples]
+testX, testY = x[numTrainExamples:], y[numTrainExamples:]
+
+numLSTMBlocks = 4
+
+# 2c - create and fit lstm network
+model = Sequential()
+model.add(LSTM(numLSTMBlocks, input_shape=(1, maxLength*6)))
+model.add(Dense(2))
+model.compile(loss='mean_squared_error', optimizer='adam')
+for i in range(100):
+  model.fit(trainX, trainY, epochs=1, batch_size=10, verbose=2, shuffle=False)
+  model.reset_states()
+
+# make predictions
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+
+# # invert predictions
+# trainPredict = scaler.inverse_transform(trainPredict)
+# trainY = scaler.inverse_transform([trainY])
+# testPredict = scaler.inverse_transform(testPredict)
+# testY = scaler.inverse_transform([testY])
+
+# # calculate root mean squared error
+# trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+# print('Train Score: %.2f RMSE' % (trainScore))
+# testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+# print('Test Score: %.2f RMSE' % (testScore))
 
 
 
-# model = Sequential()
 
-# model.add(LSTM(32, return_sequences=True, input_shape=(None, 1)))
-# model.add(LSTM(8, return_sequences=True))
-# model.add(TimeDistributed(Dense(2, activation='sigmoid')))
 
-# print(model.summary(90))
-
-# model.compile(loss='categorical_crossentropy',
-#               optimizer='adam')
-
-# model.fit(x=allData, y=allLabels, batch_size=None, epochs=10, steps_per_epoch=10, verbose=1)
